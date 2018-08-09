3aa616969b77275b83cd43c1087c1b13d0b2ef78
Azure99 <961523404@qq.com>
2018-08-09 23:36:56 +0800

    New page layout

--- RssToKindle/Controller/KindleHtmlPageBuilder.cs
+++ RssToKindle/Controller/KindleHtmlPageBuilder.cs
@@ -1,56 +1,27 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 using RssToKindle.Model;
 
 namespace RssToKindle.Controller
 {
     class KindleHtmlPageBuilder
     {
-        private StringBuilder _index;
-        private StringBuilder _body;
-        private int _count;
+        private int _classCount;//共有多少分类
+        private Dictionary<string, ClassBuilder> _dic;
         public KindleHtmlPageBuilder()
         {
-            _index = new StringBuilder();
-            _body = new StringBuilder();
-            _count = 0;
+            _classCount = 0;
+            _dic = new Dictionary<string, ClassBuilder>();
         }
 
-        /// <summary>
-        /// 添加新闻
-        /// </summary>
-        /// <param name="newsBody"></param>
         public void AddNews(NewsBody newsBody)
         {
-            int count = _count++;
+            if(!_dic.ContainsKey(newsBody.Class))
+            {
+                _dic.Add(newsBody.Class, new ClassBuilder(newsBody.Class, _classCount++));
+            }
 
-            _index.AppendLine(string.Format("<div id=\"idiv{0}\">", count));
-            _index.AppendLine(string.Format("<a href=\"#div{0}\">" +
-                "<font size=\"5\">{1}</font>" +
-                "</a>", count, newsBody.Title));
-            _index.AppendLine("<p style=\"font-size:13px;\">" + newsBody.Description + "</p>");
-            _index.AppendLine("</div>");
-            _index.AppendLine("<br/>");
-
-            _body.AppendLine(string.Format("<div id=\"div{0}\">", count));
-            _body.AppendLine("<h1>" + newsBody.Title + "</h1>");
-
-            //文章分类、当前第几篇、返回链接
-            _body.Append("<p>" + newsBody.Class);
-            _body.Append("&nbsp");
-            _body.Append("第" + (count + 1) + "篇");
-            _body.Append("&nbsp;&nbsp;");
-            _body.Append(string.Format("<a href=\"#idiv{0}\">" +
-                "<font size=\"5\">返回</font>" +
-                "</a>", count));
-            _body.AppendLine("</p>");
-
-            _body.AppendLine(newsBody.Content);
-            _body.AppendLine("<br/>");
-            _body.AppendLine("<br/>");
-            _body.AppendLine("<br/>");
-            _body.AppendLine("<br/>");
-
-            _body.AppendLine("</div>");
+            _dic[newsBody.Class].AddNews(newsBody);
         }
 
         /// <summary>
@@ -67,8 +38,7 @@ namespace RssToKindle.Controller
             sb.AppendLine("<meta charset=\"utf-8\"/>");
             sb.AppendLine("<body>");
 
-            sb.Append(_index.ToString());
-            sb.Append(_body.ToString());
+            sb.AppendLine(BuildBody());
 
             sb.AppendLine("</body>");
             sb.AppendLine("</head>");
@@ -76,9 +46,100 @@ namespace RssToKindle.Controller
 
             return sb.ToString();
         }
-        private class SingleClassBuilder
-        {
 
+        private string BuildBody()
+        {
+            StringBuilder classSb = new StringBuilder();
+            StringBuilder indexSb = new StringBuilder();
+            StringBuilder bodySb = new StringBuilder();
+
+            classSb.AppendLine("<div id=\"main\">");
+            classSb.AppendLine("<h1>RSS To Kindle</h1>");
+            classSb.AppendLine("<p>" + System.DateTime.Now.ToShortDateString() + "</p>");
+            classSb.AppendLine("<br/><br/>");
+
+            foreach(string name in _dic.Keys)
+            {
+                ClassBuilder cb = _dic[name];
+
+                classSb.Append(string.Format("<a href=\"#class{0}\">" +
+                    "<font size=\"5\">{1}({2})</font>" +
+                    "</a>", cb.ID, cb.Name, cb.Count));
+                classSb.AppendLine("<br/>");
+                classSb.AppendLine("<br/>");
+
+                indexSb.AppendLine("<hr/>");
+                indexSb.AppendLine(string.Format("<div id=\"class{0}\">", cb.ID));
+                indexSb.AppendLine("<h1>" + name + "</h1>");
+                indexSb.AppendLine("<center><a href=\"#main\"><font size=\"5\">返回</font></a></center>");
+                indexSb.AppendLine(cb.GetIndexHtml());
+
+                indexSb.AppendLine("</div>");
+
+                bodySb.AppendLine(cb.GetBodyHtml());
+            }
+
+            classSb.AppendLine("</div>");
+            classSb.AppendLine("<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>");
+
+            string body = classSb.ToString() + indexSb.ToString() + bodySb.ToString();
+            return body;
+        }
+        private class ClassBuilder
+        {
+            private StringBuilder _index { get; }
+            private StringBuilder _body { get; }
+            public int Count { get; private set; }
+            public string Name { get; }
+            public int ID { get; }//此分类的唯一ID
+
+            public ClassBuilder(string name, int id)
+            {
+                _index = new StringBuilder();
+                _body = new StringBuilder();
+                Count = 0;
+                Name = name;
+                ID = id;
+            }
+
+            public void AddNews(NewsBody newsBody)
+            {
+                int count = Count++;
+
+                _index.AppendLine(string.Format("<a href=\"#{0}div{1}\">" +
+                    "<font size=\"5\">{2}</font>" +
+                    "</a>", ID,count, newsBody.Title));
+                _index.AppendLine("<p style=\"font-size:13px;\">" + newsBody.Description + "</p>");
+                _index.AppendLine("<br/>");
+
+                _body.AppendLine(string.Format("<div id=\"{0}div{1}\">", ID, count));
+                _body.AppendLine("<h1>" + newsBody.Title + "</h1>");
+
+                //文章分类、当前第几篇、返回链接
+                _body.Append("<p>" + newsBody.Class);
+                _body.Append("&nbsp");
+                _body.Append("第" + (count + 1) + "篇");
+                _body.Append("&nbsp;&nbsp;");
+                _body.Append(string.Format("<a href=\"#class{0}\">" +
+                    "<font size=\"5\">返回</font>" +
+                    "</a>", ID));
+                _body.AppendLine("</p>");
+
+                _body.AppendLine(newsBody.Content);
+                _body.AppendLine("<br/><br/><br/><br/>");
+
+                _body.AppendLine("</div>");
+            }
+
+            public string GetIndexHtml()
+            {
+                return _index.ToString() + "<br/><br/><br/>";
+            }
+
+            public string GetBodyHtml()
+            {
+                return _body.ToString();
+            }
         }
     }
 }
